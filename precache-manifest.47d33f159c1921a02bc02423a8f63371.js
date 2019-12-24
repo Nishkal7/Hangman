ff85576479097985e1021cb33e2e0231e7ee9cdc
Nishkal <nishkal8463@gmail.com>
2019-12-24 14:22:57 +0530

    Updates

---
+++ precache-manifest.47d33f159c1921a02bc02423a8f63371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b46f0e4062af3a275ca861f5805808",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "6e71c118a432c52c6b86",
+    "url": "/Hangman/static/css/main.2da23ccc.chunk.css"
+  },
+  {
+    "revision": "3a45dbcedf0ca26bb7a9",
+    "url": "/Hangman/static/js/2.7390cd9b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Hangman/static/js/2.7390cd9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e71c118a432c52c6b86",
+    "url": "/Hangman/static/js/main.3472543a.chunk.js"
+  },
+  {
+    "revision": "0e6b67ea37171b063474",
+    "url": "/Hangman/static/js/runtime-main.fa33b032.js"
+  }
+]);
